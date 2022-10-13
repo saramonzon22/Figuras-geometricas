5b73e287093c5753f8cb06f1d2cb2a789807e05d
saramonzon22 <sara.mon.quesada@gmail.com>
2022-10-13 15:58:00 +0100

    first page structure

---
+++ README.md
@@ -0,0 +1,8 @@
+# Curso de Repaso de JS
+
+## Taller 1: Figuras geométricas
+
+- Primer paso: Definir las fórmulas
+- Segundo paso: Hacerlas funcionar en JS
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar JS con HTML
---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict'
+
+console.log('watsapp dude');
+
+// código del cuadrado
+
+function perimetroSquare(lado) {
+    return lado * 4;
+}
+perimetroSquare();
+
+function areaSquare(lado) {
+    return lado * lado;
+}
+areaSquare();
+
+
+// código del triángulo
+
+function perimetroTriangle(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+}
+perimetroTriangle();
+
+function areaTriangle(base, altura) {
+    return (base * altura) / 2;
+}
+areaTriangle();
+
+
+// código del círculo 
+
+function diametroCirculo(radio) {
+    return radio * 2;
+}
+diametroCirculo();
+
+const PI = Math.PI;
+console.log('El pi es ' + PI);
+
+
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+}
+perimetroCirculo();
+
+
+function areaCirculo(radio) {
+    return (radio * radio) * PI;
+}
+areaCirculo();
+
+
+
+
